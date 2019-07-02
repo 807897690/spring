283b1c72cde1a6f924911ddf06e6f9cc6982ea01
wzy <18357103296@163.com>
2019-07-02 09:38:51 +0800

    init

---
+++ src/main/java/com/wzy/autowire/IndexDaoImpl0.java
@@ -0,0 +1,17 @@
+package com.wzy.autowire;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * @ClassName IndexDaoImpl0
+ * @Desc TODO
+ * @Author Administrator
+ * @Date 2019/7/2 9:18
+ **/
+@Repository
+public class IndexDaoImpl0 implements IndexDao{
+
+    public void test() {
+        System.out.println("impl0");
+    }
+}
---
+++ src/main/java/com/wzy/generator/Example.java
@@ -0,0 +1,16 @@
+package com.wzy.generator;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName Example
+ * @Desc TODO
+ * @Author Administrator
+ * @Date 2019/7/2 9:08
+ **/
+@Component(value = "example1")
+public class Example {
+
+
+
+}
