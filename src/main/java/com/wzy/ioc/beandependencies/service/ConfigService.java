a52f11ff32d5a2a23d397b75180b273a14ecd099
LAPTOP-H80LEQ12\王仲沅 <18357103296@163.com>
2019-07-17 21:30:22 +0800

    mybatis log

--- src/main/java/com/wzy/ioc/beandependencies/service/ConfigService.java
+++ src/main/java/com/wzy/ioc/beandependencies/service/ConfigService.java
@@ -15,6 +15,7 @@ import java.util.Map;
  * @description:
  * mybatis-spring:一级缓存失效原因：由于spring在做完一次操作后，会将SqlSessionFactory关闭
  * 二级缓存指的是多个线程共享:二级缓存是根据命名空间隔离的，所以更新时只会更新当前命名空间下的缓存
+ * mybatis中解析mapper的方式有4中：url、import、class、package
  * @date 2019/7/17 15:09
  */
 @Service
